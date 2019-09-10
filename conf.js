9cdf678f6c02feaf8afe7be59f9253612e4fbbfe
Niels Lam <mail@nielslam.nl>
2019-09-10 17:39:41 +0200

    Code cleanup

--- conf.js
+++ conf.js
@@ -1,9 +1,7 @@
-const seleniumServer = require('selenium-server-standalone-jar');
 const JasmineConsoleReporter = require('jasmine-console-reporter');
 
 exports.config = {
     framework: 'jasmine2',
-   // seleniumServerJar: seleniumServer.path,
     baseUrl: 'https://ibis4example.ibissource.org/iaf/gui',
     directConnect: true,
 
@@ -25,16 +23,12 @@ exports.config = {
     allScriptsTimeout: 120000,
     jasmineNodeOpts: {
         defaultTimeoutInterval: 30000000
-    },
-
-    //SELENIUM_PROMISE_MANAGER: false,
-    
+    },    
     onPrepare: function(){
         jasmine.getEnv().addReporter(new JasmineConsoleReporter({
             emoji: true 
         }));
         jasmine.getEnv().defaultTimeoutInterval = 120000;
-
         browser.get('#/status');
     }
 }
