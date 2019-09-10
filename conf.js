a5d69daf964fd33395033d297b77b0503f6fbe91
Niels Lam <mail@nielslam.nl>
2019-09-10 14:58:02 +0200

    Oops...

--- conf.js
+++ conf.js
@@ -5,7 +5,7 @@ exports.config = {
     framework: 'jasmine2',
     seleniumServerJar: seleniumServer.path,
     baseUrl: 'https://ibis4example.ibissource.org/iaf/gui',
-    directconnect: true,
+    directConnect: true,
 
     capabilities: {
         browserName: 'chrome',
