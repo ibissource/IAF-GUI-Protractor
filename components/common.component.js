601b70ed677614c672781dfa12c16c92666a8803
Niels Lam <mail@nielslam.nl>
2019-09-17 15:34:57 +0200

    Fix binding according to styleguide

--- components/common.component.js
+++ components/common.component.js
@@ -3,7 +3,7 @@ var Common = function () {
     this.feedbackModal = element(by.css("div.modal-content"));
     this.feedbackClose = this.feedbackModal.element(by.css("button.btn.btn-white"));
 
-    this.serverTime = element(by.css(".nav .serverTime"));
+    this.serverTime = element(by.binding("serverTime"));
 };
 
 module.exports = Common;
