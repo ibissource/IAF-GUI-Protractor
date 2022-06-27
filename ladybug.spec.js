9e8e421d7ed7ed1405011b79e9227a64ad2eb96c
Martijn Dirkse <martijn@integrationpartners.nl>
2022-06-27 10:58:35 +0200

    Fix tests

--- ladybug.spec.js
+++ ladybug.spec.js
@@ -47,19 +47,19 @@ describe('Ladybug Page tests', function(){
 		prepare();
 		ladybug.debugTab.click();
 		ladybug.enableReportGenerator();
-		// The test depends on the existence of many Ladybug reports. Create them.
+		// Ideally, tests should not depend on each other. This ideal cannot
+		// be implemented however because we cannot remove Ladybug reports from
+		// the table in the test tab.
+		//
+		// Test "Should list the correct number of reports in the storage when input a number"
+		// configures that at most 3 reports should be visible and expects to have 3 reports then.
+		// This test only makes sense if there are 4 or more reports during that test.
+		//
+		// Test "When I enable the report generator, test a pipeline and then refresh, should appear a new report in the storage"
+		// creates one Ladybug report. We need to create 3 more to have meaningful tests.
 		testAPipeline("msg 1");
 		testAPipeline("msg 2");
 		testAPipeline("msg 3");
-		testAPipeline("msg 4");
-		testAPipeline("msg 5");
-		testAPipeline("msg 6");
-		testAPipeline("msg 7");
-		testAPipeline("msg 8");
-		testAPipeline("msg 9");
-		testAPipeline("msg 10");
-		testAPipeline("msg 11");
-		testAPipeline("msg 12");
 		ladybug.debugTab.click();
 	});
 
@@ -70,7 +70,7 @@ describe('Ladybug Page tests', function(){
 		teardown();
 	})
 
-	it('When I enable the report generator, test a pipeline and then refresh, should appear a new report in the storage ', function() {
+	it('When I enable the report generator, test a pipeline and then refresh, should appear a new report in the storage', function() {
 		ladybug.refreshDebug.click();
 		ladybug.enableReportGenerator();
 		// First test a pipeline in the Test Pipeline page, then check for the result in Ladybug
@@ -93,12 +93,18 @@ describe('Ladybug Page tests', function(){
 	});
 	
 	it('Should list the correct number of reports in the storage when input a number', function() {
+		expect(ladybug.reportsInStorage.count()).toBeGreaterThanOrEqual(5);
 		ladybug.numOfReportsVisible.clear().sendKeys(3);
+		// The sendKey command did not do enter. Using the entered value can also be done by pressing refresh.
 		ladybug.refreshDebug.click();
 		browser.wait(EC.invisibilityOf(ladybug.reportsInStorage.get(4)), 3000, 'timeouts, numOfReportsVisible is more than 3');
+		// Include the header row
 		expect(ladybug.reportsInStorage.count()).toBe(4);
 		// Set it back to default value
 		ladybug.numOfReportsVisible.clear().sendKeys(10);
+		ladybug.refreshDebug.click();
+		browser.wait(EC.visibilityOf(ladybug.reportsInStorage.get(4)), 3000, 'The fourth report did not become visible');
+		expect(ladybug.reportsInStorage.count()).toBeGreaterThanOrEqual(5);
 		ladybug.refreshDebug.click();
 	});
 	
@@ -114,7 +120,8 @@ describe('Ladybug Page tests', function(){
 		// Should open correct number of reports in the reports directory when I click Open all tab
 		ladybug.toggleReports.click();
 		browser.wait(EC.visibilityOf(ladybug.report), 3000);
-		expect(ladybug.reportsDirectory.count()).toBeGreaterThanOrEqual(1);
+		// At least four reports and a root node
+		expect(ladybug.reportsDirectory.count()).toBeGreaterThanOrEqual(5);
 		// Should close all reports in the reports directory when I click Close all tab
 		ladybug.closeAllTab.click();
 		browser.wait(EC.invisibilityOf(ladybug.report), 3000);
