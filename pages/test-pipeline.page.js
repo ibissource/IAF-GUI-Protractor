f726d4fd824dd98ffab62e41c1682fd9939a85cd
Martijn Dirkse <martijn@integrationpartners.nl>
2022-12-15 12:51:31 +0100

    Work in progress - fixing tests

--- pages/test-pipeline.page.js
+++ pages/test-pipeline.page.js
@@ -1,11 +1,16 @@
 var TestPipelinePage = function() {
-	this.adapter = element(by.css('[value="HelloWorld"]'))
+	this.configuration = element(by.cssContainingText('option', 'Frank2Example'));
+	this.adapter = element(by.cssContainingText('option', 'HelloWorld'));
 	this.messageField = element(by.model('form.message'));
 	this.btnSend = element(by.buttonText("Send"));
 	this.result = element(by.exactBinding("result"));
 	this.alertMessage = element(by.css('[role="alert"]'));
 	
 	this.testPipeline = function(message){
+		this.configuration.click();
+		let EC = protractor.ExpectedConditions;
+		browser.wait(EC.elementToBeClickable(this.adapter), 10000);
+		browser.sleep(1000);
 		this.adapter.click();
 		this.messageField.sendKeys(message);
 		this.btnSend.click();	
